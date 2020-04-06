b6a179f70623cb7bea27b432163a816fdb9f1741
阿知波雅人 <tokemdokea@yahoo.co.jp>
2020-04-07 02:16:10 +0900

    オートレイアウト 完了

--- calendarapp/ImageViewController.swift
+++ calendarapp/ImageViewController.swift
@@ -18,6 +18,7 @@ class ImageViewController: UIViewController {
   
         @IBOutlet var loveBar: UIProgressView!
         
+        @IBOutlet var doryokuLabel: CustomLabel!
         
         @IBOutlet var progressLabel: UILabel!
         
@@ -39,7 +40,8 @@ class ImageViewController: UIViewController {
         super.viewDidLoad()
      
         let talkManager = TalkManager()
-        
+//        speechText.adjustsFontSizeToFitWidth = true 効果なし
+//        speechText.minimumScaleFactor = 10
         
         switch talkManager.currentType {
         case .good:
@@ -58,12 +60,16 @@ class ImageViewController: UIViewController {
     loveBar.frame = CGRect(x:0, y: 0, width :0, height: 0) //優先度が低い
        // view.layoutIfNeeded()
         loveBar.translatesAutoresizingMaskIntoConstraints = false //制約をかける合図
-        loveBar.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        loveBar.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        loveBar.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.4).isActive = true
+        loveBar.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.04).isActive = true
        loveBar.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         loveBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
         
-        
+        doryokuLabel.translatesAutoresizingMaskIntoConstraints = false
+        doryokuLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.4).isActive = true
+        doryokuLabel.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.04).isActive = true
+        doryokuLabel.rightAnchor.constraint(equalTo: loveBar.leftAnchor, constant: -20).isActive = true
+        doryokuLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
         
         //  loveBar.transform = transform1.concatenating(transform2)
         speechText.padding = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
@@ -87,7 +93,7 @@ class ImageViewController: UIViewController {
                 loveBar.setProgress(Float(Double(talkManager.currentTotalPoint) * 0.005555), animated: true)
 
                 let num = loveBar.progress  * 100
-                progressLabel.text = "\(ceil(num * 10)/10)%"
+                doryokuLabel.text = "努力値累計  " +  "\(ceil(num * 10)/10)%"
                 
                 count.text = String(talkManager.currentTotalPoint)
                 
